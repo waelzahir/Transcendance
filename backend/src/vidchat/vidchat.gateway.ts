32732cf2dab4fe214bc153d2f7cf718f98d89500
waelzahir <ozahir@student.1337.ma>
2024-01-17 06:21:54 +0100

    w:wwww

--- backend/src/vidchat/vidchat.gateway.ts
+++ backend/src/vidchat/vidchat.gateway.ts
@@ -1,9 +1,36 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway,WebSocketServer } from '@nestjs/websockets';
+import { Server , Socket} from "socket.io";
 
-@WebSocketGateway()
+@WebSocketGateway({namespace: "hanan"})
 export class VidchatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+
+  constructor()
+  {
+    this.ids = new Map()
+  }
+  @WebSocketServer()
+	server :Server;
+  ids: Map<string, string>
+
+  async handleConnection(client)
+  {
+    this.ids.set(client.id, Date.now().toString() )
+    console.log(this.ids)
+    client.emit("MYID", this.ids.get(client.id))
+    this.server.except(client.id).emit("ONLINE", this.ids.get(client.id))
+  }
+
+  async handleDisconnect(client) {
+    console.log("disconected")
+    this.ids.delete(client.id)
+    console.log(this.ids)
+    client.emit("MYID", this.ids.get(client.id))
+    this.server.except(client.identifierd).emit("OFFLINE", this.ids.get(client.id))
+  }
+  
+  @SubscribeMessage('')
+  handleMessage(client: any, payload: any) {
+
+    client.emit("")
   }
 }
--- frontend/src/videochat/Widow.tsx
+++ frontend/src/videochat/Widow.tsx
@@ -1,24 +1,97 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
+import { Socket, io } from "socket.io-client";
 import IUser from "../types/User"
 import { ip } from "../network/ipaddr"
 import { toast } from "react-toastify"
+import { AnimatedElement } from "../components/game/GameSetup";
+
+
+
+const socket: Socket = io(`ws://${ip}3001/hanan`, { autoConnect: false, transports: ["websocket"] });
+
+const UserComponent = ({id}: {id :string}) =>
+{
+    
+    return (
+        <div className={`${id === "me" ? "bg-white" : ""} h-36 border-solid border-2  flex justify-around`}>
+            <div className="flex justify-center items-center m-auto">
+                <img className="h-[100px] w-[100px] rounded-full" src="https://picsum.photos/200/300"></img>
+            </div>
+                <div className="flex-row justify-center m-auto">
+                    <h1 className="text-center">id {id}</h1>
+                    <h1 className="text-center" >nickname</h1>
+                    <h1 className="text-center" >username</h1>
+                    {
+                        id === "me" ?
+                        null
+                        :
+                        <button className="rounded-full border-2 border-solid bg-green-600 w-[50px] h-[50px]" onClick={() => {alert(id)}}>  </button>
+                    }
+                </div>
+        </div>
+    )
+}
 
 export const Widow =() =>
 {
-    const [online , setOnline] = useState<IUser>()
+    const [online , setOnline] = useState<string[]>([])
+    const [me, setme] = useState<string>("")
+    const [incall, setincall] = useState(false);
+    const outgoing = useRef()
+    const incoming = useRef()
+
     useEffect(() =>
     {
-        fetch("http://" + ip + "3001/widow").then(async res => 
-        {
-
-            if (res.ok)
-                setOnline(await res.json())
-            else
-                toast.error("widow service error");
+        socket.connect();
+        socket.on("MYID",  (data) => setme(data))
+        socket.on("ONLINE", (data) => {
+            const ndata = online.slice()
+            ndata.unshift(data)
+            setOnline(ndata)
+        })
+        socket.on("OFFLINE", (data) => {
+            const ndata = online.filter(nd =>  nd != data)
+            setOnline(ndata)
+        })
+        return () =>{
+            socket.off("MYID")
+            socket.off("ONLINE")
+            socket.off("OFFLINE")
+            socket.disconnect()
         }
-        )
     },[])
-    
-    console.log(online)
-    return (<div>suck dick </div>)
+    let userlist;
+    if (online)
+        userlist = online.map((user) =>  <UserComponent key={user} id={user}/> )
+    return (
+            <div className="flex justify-between w-full h-full bg-slate-600 m-auto">
+            
+                <div className=" flex-row overflow-y-auto w-2/12 border-solid border-2 gap-y-4">
+                        <UserComponent id="me"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                        <UserComponent id="mamak"/>
+                </div>
+                {
+                    incall ? 
+                    <div className="flex justify-center  items-center gap-x-4 m-auto">
+                        <video className="bg-black" width="854" height="480"></video>
+                       <video  className="bg-white" width="854" height="480"></video>
+                </div>
+                :
+                <AnimatedElement />
+                }
+            </div>
+        )
 }
