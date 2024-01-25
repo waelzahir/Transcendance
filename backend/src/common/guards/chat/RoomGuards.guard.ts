cfa789772bdfcf3cbea83931abbb2ab5ba024bff
waelzahir <ozahir@student.1337.ma>
2024-01-25 17:06:35 +0100

    fixes

--- backend/src/common/guards/chat/RoomGuards.guard.ts
+++ backend/src/common/guards/chat/RoomGuards.guard.ts
@@ -40,6 +40,7 @@ export class RoomGuard implements CanActivate {
         {
             context.switchToWs().getClient().request.headers["user"] = request.user[key2];
             roomid = +context.switchToWs().getData().room;
+            console.log(roomid)
         }
         else
             roomid = +request.query["room"]
@@ -49,7 +50,10 @@ export class RoomGuard implements CanActivate {
         {
             
                 if (Number.isNaN(roomid))
+                {
+                    context.switchToWs().getClient().emit("ChatError", "wrong id")
                     return false;
+                }
                 const frdbroom = await this.prisma.rooms.findUnique({where: { id: roomid },});
                 if (!frdbroom)
                 {
